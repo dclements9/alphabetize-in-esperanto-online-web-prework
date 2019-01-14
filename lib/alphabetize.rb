300c33b478a32171558c745877fb6c0572c59e32
Dylan Clements <dclements@protonmail.com>
2019-01-14 02:16:35 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,8 +4,10 @@ def alphabetize(arr)
 esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |phrase|
     phrase.chars.map do |character|
-      binding.pry
+      #binding.pry
+      test = esperanto_alphabet.index(character)
       esperanto_alphabet.index(character)
+      binding.pry
     end
   end
 end
